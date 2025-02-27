3f9ac80697b838e071b446d84853a57219932f19
SkhtskIryna <foxira242@gmail.com>
2025-02-27 21:45:57 +0200

    Addition User Module

--- user-service/src/modules/modules.module.ts
+++ user-service/src/modules/modules.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+//import { AuthModule } from './auth/auth.module';
+//import { OrmModule } from './orm/orm.module';
 
-@Module({})
+@Module({ imports: [/*AuthModule, OrmModule,*/ UserModule],})
 export class ModulesModule {}
