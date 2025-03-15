db1ec11279001e933a414296935ba60487ba7071
Tania Holub <taniagolub2004@gmail.com>
2025-03-15 16:00:07 +0200

    modified patterns in user-service

--- user-service/src/modules/user/user.controller.ts
+++ user-service/src/modules/user/user.controller.ts
@@ -7,7 +7,7 @@ import { patterns } from '../patterns';
 @Controller()
 export class userController {
   private readonly logger = new Logger(userController.name);
-  constructor(private readonly userService: userService) {}
+  constructor(private readonly userService: userService) { }
 
   @MessagePattern(patterns.USER.CREATE)
   async createUser(data: userDTO) {
@@ -15,38 +15,38 @@ export class userController {
     return this.userService.createUser(data);
   }
 
-  @MessagePattern({ cmd: 'login' })
-  async loginMember(data: userDTO){
+  @MessagePattern(patterns.AUTH.LOGIN)
+  async loginMember(data: userDTO) {
     this.logger.log('Login user');
     return this.userService.loginUser(data);
   }
 
-  @MessagePattern({ cmd: 'find_user_by_id' })
+  @MessagePattern(patterns.USER.FIND_BY_ID)
   async findUserById(dto) {
     return this.userService.findUserById(dto);
   }
 
-  @MessagePattern({ cmd: 'find_user_by_email' })
+  @MessagePattern(patterns.USER.FIND_BY_EMAIL)
   async findUserByEmail(email: string) {
     return this.userService.findUserByEmail(email);
   }
 
-  @MessagePattern({ cmd: 'delete_user' })
+  @MessagePattern(patterns.USER.DELETE)
   async deleteUser(id: string) {
     return this.userService.deleteUser(id);
   }
 
-  @MessagePattern({ cmd: 'update_user' })
+  @MessagePattern(patterns.USER.UPDATE)
   async updateUser(data: { id: string; dto: userDTO }) {
     return this.userService.updateUser(data.id, data.dto);
   }
 
-  @MessagePattern({ cmd: 'reset_password' })
+  @MessagePattern(patterns.USER.RESET_PASSWORD)
   async resetPassword(email: string) {
     return this.userService.resetPassword(email);
   }
 
-  @MessagePattern({ cmd: 'find_all_users' })
+  @MessagePattern(patterns.USER.FIND_ALL)
   async listUsers() {
     return this.userService.listUsers();
   }
